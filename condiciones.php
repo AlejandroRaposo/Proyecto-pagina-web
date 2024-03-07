70c3440335322dfbe8de63e70ebdaf1348381fbd
daweb048 <daweb048@ejemplo.com>
2024-03-07 12:59:20 +0100

    cambiado el link de scss

--- condiciones.php
+++ condiciones.php
@@ -7,7 +7,7 @@
   <title>Salud express - condiciones</title>
   <link rel="shortcut icon" href="assets/img/caduceo16.png" type="image/x-icon" autor="Freepik">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/SCSS/sass.css">
 </head>
 
 <body>
